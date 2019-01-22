79bab11df7b5a03acea228e564685eea03605470
Gaël Renoux <gael.renoux@gmail.com>
2019-01-22 19:40:42 +0100

    Better launcher for tests. Logs a past schedule.

--- src/test/scala/fr/renoux/gaston/command/MainLauncher.scala
+++ src/test/scala/fr/renoux/gaston/command/MainLauncher.scala
@@ -2,15 +2,25 @@ package fr.renoux.gaston.command
 
 object MainLauncher extends App {
 
-  Main.main(Array(
+  def displaySample(): Unit = Main.main(Array(
     "--from-sample",
-    //"--from",
-    //"/home/gael/Temp/UdoCon/udocon_2018_settings.conf",
-    //"--from-udo-table",
-    //"/home/gael/Temp/UdoCon/udocon_2018_table.conf",
     "--generate-input",
-    "--debug"
+    //"--debug",
     //"--silent"
   ))
 
+  def displayUdoCon2108(): Unit = Main.main(Array(
+    "--from",
+    "/home/gael/Temp/UdoCon/udocon_2018_settings.conf",
+    "--from-udo-table",
+    "/home/gael/Temp/UdoCon/udocon_2018_table.conf",
+    "--generate-input",
+  ))
+
+  def runOnRealCase(): Unit = Main.main(Array(
+    "--from",
+    "src/test/resources/udocon-2017-completed.conf"
+  ))
+
+  runOnRealCase()
 }
