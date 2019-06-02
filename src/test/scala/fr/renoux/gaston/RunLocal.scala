43afac9a534bbeab648ab3921da15fbd9fbccbd5
Gaël Renoux <gael.renoux@gmail.com>
2019-06-02 14:37:46 +0200

    Only one launcher needed

--- src/test/scala/fr/renoux/gaston/RunLocal.scala
+++ src/test/scala/fr/renoux/gaston/RunLocal.scala
@@ -4,6 +4,31 @@ import fr.renoux.gaston.command.Main
 
 object RunLocal extends App {
 
-  Main.main(Array("-f", "/home/gael/Repositories/Gael/gaston/src/test/resources/udocon2017/completed.conf"))
+  def displaySample(): Unit = Main.main(Array(
+    "--from-sample",
+    "--generate-input",
+    //"--debug",
+    //"--silent"
+  ))
 
+  def displayUdoCon2017(): Unit = Main.main(Array(
+    "--from-table",
+    "/home/gael/Repositories/Gael/gaston/src/test/resources/udocon2017/uc17-table.csv",
+    "--generate-input",
+  ))
+
+  def displayUdoCon2019(): Unit = Main.main(Array(
+    "--from",
+    "/home/gael/Repositories/Gael/gaston/src/test/resources/udocon2019/uc19-settings.conf",
+    "--from-table",
+    "/home/gael/Repositories/Gael/gaston/src/test/resources/udocon2019/uc19-table.csv",
+    "--generate-input",
+  ))
+
+  def runOnRealCase(): Unit = Main.main(Array(
+    "--from",
+    "src/test/resources/udocon2017/uc17-completed.conf"
+  ))
+
+  displayUdoCon2019()
 }
