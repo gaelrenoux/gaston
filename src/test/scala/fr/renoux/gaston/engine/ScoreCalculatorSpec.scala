23ec795592c732d342fa039ac53723ae804e7fd6
Gaël Renoux <gael.renoux@gmail.com>
2021-06-27 16:45:45 +0200

    Adds tests on ScoreCalculator

--- src/test/scala/fr/renoux/gaston/engine/ScoreCalculatorSpec.scala
+++ src/test/scala/fr/renoux/gaston/engine/ScoreCalculatorSpec.scala
@@ -1,11 +1,42 @@
 package fr.renoux.gaston.engine
 
 import fr.renoux.gaston.SimpleTestModel
+import fr.renoux.gaston.model.Score
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class ScoreCalculatorSpec extends AnyFlatSpec with Matchers {
+
+
   "score" should "give the correct result for a simple problem" in {
     SimpleTestModel.Solutions.Best.score.value should be(10.2265625)
+  }
+
+  it should "give a correct unweightedScoresByPerson" in {
+    SimpleTestModel.Solutions.Best.scoreCalculator.unweightedScoresByPerson should be(Map(
+      SimpleTestModel.Persons.Arthur -> Score(5.0),
+      SimpleTestModel.Persons.Bianca -> Score(6.0),
+      SimpleTestModel.Persons.Corwin -> Score(6.0),
+      SimpleTestModel.Persons.Daniela -> Score(6.0),
+      SimpleTestModel.Persons.Eric -> Score(5.0),
+      SimpleTestModel.Persons.Fiona -> Score(6.0),
+      SimpleTestModel.Persons.Garion -> Score(6.0),
+      SimpleTestModel.Persons.Hercule -> Score(6.0),
+      SimpleTestModel.Persons.Iago -> Score(5.0),
+    ))
+  }
+
+  it should "give a correct weightedScoresByPerson" in {
+    SimpleTestModel.Solutions.Best.scoreCalculator.weightedScoresByPerson should be(Map(
+      SimpleTestModel.Persons.Arthur -> Score(5.0),
+      SimpleTestModel.Persons.Bianca -> Score(6.0),
+      SimpleTestModel.Persons.Corwin -> Score(6.0),
+      SimpleTestModel.Persons.Daniela -> Score(6.0),
+      SimpleTestModel.Persons.Eric -> Score(5.0),
+      SimpleTestModel.Persons.Fiona -> Score(6.0),
+      SimpleTestModel.Persons.Garion -> Score(6.0),
+      SimpleTestModel.Persons.Hercule -> Score(6.0),
+      SimpleTestModel.Persons.Iago -> Score(5.0),
+    ))
   }
 }
