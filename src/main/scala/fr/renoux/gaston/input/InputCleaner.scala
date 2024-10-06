aaaf78cc56a172e34dcd45ce3eb0d5567df1ab6b
Gaël Renoux <gael.renoux@gmail.com>
2024-10-06 18:42:44 +0200

    Fix InputCleaner

--- src/main/scala/fr/renoux/gaston/input/InputCleaner.scala
+++ src/main/scala/fr/renoux/gaston/input/InputCleaner.scala
@@ -3,95 +3,104 @@ package fr.renoux.gaston.input
 import cats.data.NonEmptyList
 import eu.timepit.refined.api.Refined
 import fr.renoux.gaston.model.{Record, Score, Weight}
-import fr.renoux.gaston.util.StringImplicits._
-import shapeless._
+import fr.renoux.gaston.util.StringImplicits.*
 
+import scala.annotation.targetName
 import scala.concurrent.duration.FiniteDuration
 
 /** Typeclass: can be cleaned up. Used on the input to sanitize entries: remove special characters from Strings, for
   * example. */
 trait InputCleaner[A] {
-  def clean(a: A): A
+  extension (a: A) {
+    def clean: A
+  }
 }
 
 object InputCleaner {
 
-  def apply[A](implicit ci: InputCleaner[A]): InputCleaner[A] = ci
+  def apply[A](using ci: InputCleaner[A]): InputCleaner[A] = ci
 
   def instance[A](f: A => A): InputCleaner[A] = new InputCleaner[A] {
-    override def clean(a: A): A = f(a)
+    extension (a: A) {
+      override def clean: A = f(a)
+    }
   }
 
-  implicit object StringIsCleanableInput extends InputCleaner[String] {
-    override def clean(a: String): String = a.trim
-      .replaceRec(Record.FormattedTopicPersonsSeparator, Record.FormattedTopicPersonsSeparator.tail)
-      .replaceRec(Record.FormattedPersonsSeparator, Record.FormattedPersonsSeparator.tail)
-      .replaceRec(Record.MandatoryMarker, Record.MandatoryMarker.toLowerCase)
+  def identityCleaner[A] = new InputCleaner[A] {
+    extension (a: A) {
+      override def clean: A = a
+    }
   }
 
-  class IdentityCleaner[A] extends InputCleaner[A] {
-    override def clean(a: A): A = a
+  given InputCleaner[String] with {
+    extension (a: String) {
+      override def clean = a.trim
+        .replaceRec(Record.FormattedTopicPersonsSeparator, Record.FormattedTopicPersonsSeparator.tail)
+        .replaceRec(Record.FormattedPersonsSeparator, Record.FormattedPersonsSeparator.tail)
+        .replaceRec(Record.MandatoryMarker, Record.MandatoryMarker.toLowerCase)
+    }
   }
 
-  implicit object BooleanCleaner extends IdentityCleaner[Boolean]
+  given InputCleaner[Boolean] = identityCleaner[Boolean]
 
-  implicit object IntCleaner extends IdentityCleaner[Int]
+  given InputCleaner[Int] = identityCleaner[Int]
 
-  implicit object LongCleaner extends IdentityCleaner[Long]
+  given InputCleaner[Long] = identityCleaner[Long]
 
-  implicit object DoubleCleaner extends IdentityCleaner[Double]
+  given InputCleaner[Double] = identityCleaner[Double]
 
-  implicit object FiniteDurationCleaner extends IdentityCleaner[FiniteDuration]
+  given InputCleaner[FiniteDuration] = identityCleaner[FiniteDuration]
 
-  implicit final def optionInputCleaner[A](implicit aCleaner: InputCleaner[A]): InputCleaner[Option[A]] =
-    instance(option => option.map(aCleaner.clean))
+  given [A: InputCleaner]: InputCleaner[Option[A]] =
+    instance(option => option.map(_.clean))
 
-  implicit final def eitherInputCleaner[A, B](implicit aCleaner: InputCleaner[A], bCleaner: InputCleaner[B]): InputCleaner[Either[A, B]] =
+  given [A: InputCleaner, B: InputCleaner]: InputCleaner[Either[A, B]] =
     instance {
-      case Left(a) => Left(aCleaner.clean(a))
-      case Right(b) => Right(bCleaner.clean(b))
+      case Left(a) => Left(a.clean)
+      case Right(b) => Right(b.clean)
     }
 
-  implicit final def listInputCleaner[A](implicit aCleaner: InputCleaner[A]): InputCleaner[List[A]] =
-    instance(as => as.map(aCleaner.clean))
+  given [A: InputCleaner]: InputCleaner[List[A]] =
+    instance(as => as.map(_.clean))
 
-  implicit final def nelInputCleaner[A](implicit aCleaner: InputCleaner[A]): InputCleaner[NonEmptyList[A]] =
-    instance(as => as.map(aCleaner.clean))
+  given [A: InputCleaner]: InputCleaner[NonEmptyList[A]] =
+    instance(as => as.map(_.clean))
 
-  implicit final def setInputCleaner[A](implicit aCleaner: InputCleaner[A]): InputCleaner[Set[A]] =
-    instance(as => as.map(aCleaner.clean))
+  given [A: InputCleaner]: InputCleaner[Set[A]] =
+    instance(as => as.map(_.clean))
 
-  implicit final def mapInputCleaner[K, V](implicit kCleaner: InputCleaner[K], vCleaner: InputCleaner[V]): InputCleaner[Map[K, V]] =
-    instance(map => map.map {
-      case (k, v) => kCleaner.clean(k) -> vCleaner.clean(v)
+  given [K: InputCleaner, V: InputCleaner]: InputCleaner[Map[K, V]] =
+    instance(kvs => kvs.map {
+      case (k, v) => k.clean -> v.clean
     })
+
 
   /* My model */
 
-  implicit object WeightCleaner extends IdentityCleaner[Weight]
+  given InputCleaner[Weight] = identityCleaner[Weight]
 
-  implicit object ScoreCleaner extends IdentityCleaner[Score]
+  given InputCleaner[Score] = identityCleaner[Score]
+
 
   /* Refined stuff */
 
-  implicit final def refinedInputCleaner[A, R](implicit aCleaner: InputCleaner[A]): InputCleaner[A Refined R] =
-    instance { ref =>
-      Refined.unsafeApply[A, R](aCleaner.clean(ref.value)) // TODO Very ugly, should do for now
+  given [A: InputCleaner, R]: InputCleaner[A Refined R] =
+    instance { ref => Refined.unsafeApply[A, R](ref.value.clean) } // TODO Very ugly, should do for now
+
+
+  /* Definitions for tuples */
+
+  given [A: InputCleaner]: InputCleaner[Tuple1[A]] =
+    instance { at => Tuple1(at._1.clean) }
+
+  @targetName("NTupleInputCleaner")
+  given [A: InputCleaner, B <: Tuple : InputCleaner]: InputCleaner[A *: B] with {
+    extension (ab: A *: B) {
+      override def clean: A *: B = {
+        val a *: b = ab
+        a.clean *: b.clean
+      }
     }
-
-  /* Shapeless definition for case classes */
-
-  implicit object HnilInputCleaner extends InputCleaner[HNil] {
-    override def clean(a: HNil): HNil = a
   }
 
-  implicit final def hListInputCleaner[H, T <: HList](implicit hCleaner: InputCleaner[H], tCLeaner: InputCleaner[T]): InputCleaner[H :: T] =
-    instance { case h :: t => hCleaner.clean(h) :: tCLeaner.clean(t) }
-
-  implicit final def genericInputCleaner[A, R](implicit gen: Generic.Aux[A, R], enc: InputCleaner[R]): InputCleaner[A] =
-    instance(a => gen.from(enc.clean(gen.to(a))))
-
-  implicit final class InputCleanerOps[A](wrapped: A)(implicit aCleaner: InputCleaner[A]) {
-    @inline def clean(): A = aCleaner.clean(wrapped)
-  }
 }
