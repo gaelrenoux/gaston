f013ebfe2157c4b49e0e9a5263e2bfd4f9e1a9de
Gaël Renoux <gael.renoux@gmail.com>
2024-11-12 19:40:30 +0100

    Advance a bit

--- src/main/scala/fr/renoux/gaston/model2/Schedule.scala
+++ src/main/scala/fr/renoux/gaston/model2/Schedule.scala
@@ -1,9 +1,10 @@
 package fr.renoux.gaston.model2
 
 class Schedule(
-    val content: Matrix[PersonId, SlotId, TopicId]
+    val content: Matrix3[SlotId, TopicId, PersonId, Boolean]
 ) {
-  lazy val personGroups: IdMap[PersonId, SmallIdSet[PersonId]] = ???
+  lazy val personTopics: IdMap[PersonId, SmallIdSet[TopicId]] = ???
+  lazy val topicsToPersons: IdMap[TopicId, SmallIdSet[PersonId]] = ???
   lazy val topicsPresent: SmallIdSet[TopicId] = ???
 
 }
