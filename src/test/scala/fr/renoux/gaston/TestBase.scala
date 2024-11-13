b3f6ef49b627ed8dcb3a79932b21503c181c5ada
Gaël Renoux <gael.renoux@gmail.com>
2024-11-13 22:02:31 +0100

    Adds some tests and advance a bit

---
+++ src/test/scala/fr/renoux/gaston/TestBase.scala
@@ -0,0 +1,6 @@
+package fr.renoux.gaston
+
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.should.Matchers
+
+abstract class TestBase extends AnyFreeSpec with Matchers
---
+++ src/test/scala/fr/renoux/gaston/model2/ScoreWeightTest.scala
@@ -0,0 +1,13 @@
+package fr.renoux.gaston.model2
+
+import fr.renoux.gaston.TestBase
+
+class ScoreWeightTest extends TestBase {
+
+  "Score*Weight" in {
+    val s = Score(100)
+    val w = Weight(0.5)
+    (s * w) should be(Score(50))
+    (w * s) should be(Score(50))
+  }
+}
