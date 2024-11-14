92b62fc66872984fa63024eac2bbba039870ffe3
Gaël Renoux <gael.renoux@gmail.com>
2024-11-14 19:01:09 +0100

    More tests on SmallIdSet, and bugfix

--- src/test/scala/fr/renoux/gaston/model2/SmallIdSetTest.scala
+++ src/test/scala/fr/renoux/gaston/model2/SmallIdSetTest.scala
@@ -10,104 +10,192 @@ class SmallIdSetTest extends TestBase {
   val testKoInts: Seq[Int] = (testAllInts.toSet -- testOkInts).toList
   val testKoIds: Seq[TopicId] = testKoInts.map(TopicId(_))
 
-  "Creation > empty set" in {
-    val set = SmallIdSet()
-    set.underlying should be(0)
-    SmallIdSet.empty[TopicId] should be(0)
-  }
+  "Creation" - {
+    "empty set" in {
+      val set = SmallIdSet()
+      set.underlying should be(0)
+      SmallIdSet.empty[TopicId] should be(0)
+    }
 
-  "Creation > non-empty set" in {
-    val set = SmallIdSet(testOkIds *)
-    set.underlying should be(-9223231299366420216L)
-  }
+    "non-empty set" in {
+      val set = SmallIdSet(testOkIds*)
+      set.underlying should be(-9223231299366420216L)
+    }
 
-  "Creation > full set" in {
-    val set = SmallIdSet(testAllIds *)
-    set.underlying should be(-1L)
-    SmallIdSet.full[TopicId] should be(-1L)
-  }
-
-  "contains > empty set" in {
-    val set = SmallIdSet.empty[TopicId]
-    testAllIds.foreach { id =>
-      set.contains(id) should be(false)
-      set(id) should be(false)
+    "full set" in {
+      val set = SmallIdSet(testAllIds*)
+      set.underlying should be(-1L)
+      SmallIdSet.full[TopicId] should be(-1L)
     }
   }
 
-  "contains > non-empty set" in {
-    val set = SmallIdSet(testOkIds *)
-    testOkIds.foreach { id =>
-      set.contains(id) should be(true)
-      set(id) should be(true)
+  "contains" - {
+    "empty set" in {
+      val set = SmallIdSet.empty[TopicId]
+      testAllIds.foreach { id =>
+        set.contains(id) should be(false)
+        set(id) should be(false)
+      }
     }
-    testKoIds.foreach { id =>
-      set.contains(id) should be(false)
-      set(id) should be(false)
-    }
-  }
 
-  "contains > full set" in {
-    val set = SmallIdSet.full[TopicId]
-    testAllIds.foreach { id =>
-      set.contains(id) should be(true)
-      set(id) should be(true)
+    "non-empty set" in {
+      val set = SmallIdSet(testOkIds*)
+      testOkIds.foreach { id =>
+        set.contains(id) should be(true)
+        set(id) should be(true)
+      }
+      testKoIds.foreach { id =>
+        set.contains(id) should be(false)
+        set(id) should be(false)
+      }
     }
-  }
 
-  "added > on empty set" in {
-    val set = SmallIdSet.empty[TopicId]
-    for (id <- testOkIds) {
-      val s1 = set + id
-      val s2 = set.added(id)
-      s1.contains(id) should be(true)
-      s2.contains(id) should be(true)
-      testKoIds.foreach { nid =>
-        s1.contains(nid) should be(false)
-        s2.contains(nid) should be(false)
+    "full set" in {
+      val set = SmallIdSet.full[TopicId]
+      testAllIds.foreach { id =>
+        set.contains(id) should be(true)
+        set(id) should be(true)
       }
     }
   }
 
-  "added > on non-empty set > non-existing value" in {
-    val set: SmallIdSet[TopicId] = SmallIdSet(testOkIds*)
-    for (id <- testKoIds) {
-      val s1 = set + id
-      val s2 = set.added(id)
-      s1.contains(id) should be(true)
-      s2.contains(id) should be(true)
-      testKoIds.filterNot(_ == id).foreach { nid =>
-        s1.contains(nid) should be(false)
-        s2.contains(nid) should be(false)
+  "added" - {
+    "on empty set" in {
+      val set = SmallIdSet.empty[TopicId]
+      for (id <- testOkIds) {
+        val s1 = set + id
+        val s2 = set.added(id)
+        s1.contains(id) should be(true)
+        s2.contains(id) should be(true)
+        testAllIds.filterNot(_ == id).foreach { nid =>
+          s1.contains(nid) should be(false)
+          s2.contains(nid) should be(false)
+        }
+      }
+    }
+
+    "on non-empty set > non-existing value" in {
+      val set: SmallIdSet[TopicId] = SmallIdSet(testOkIds*)
+      for (id <- testKoIds) {
+        val s1 = set + id
+        val s2 = set.added(id)
+        s1.contains(id) should be(true)
+        s2.contains(id) should be(true)
+        testOkIds.foreach { nid =>
+          s1.contains(nid) should be(true)
+          s2.contains(nid) should be(true)
+        }
+        testKoIds.filterNot(_ == id).foreach { nid =>
+          s1.contains(nid) should be(false)
+          s2.contains(nid) should be(false)
+        }
+      }
+    }
+
+    "on non-empty set > existing value" in {
+      val set: SmallIdSet[TopicId] = SmallIdSet(testOkIds*)
+      for (id <- testOkIds) {
+        val s1 = set + id
+        val s2 = set.added(id)
+        s1 should be(set)
+        s2 should be(set)
+        s1.contains(id) should be(true)
+        s2.contains(id) should be(true)
+        testOkIds.filterNot(_ == id).foreach { nid =>
+          s1.contains(nid) should be(true)
+          s2.contains(nid) should be(true)
+        }
+        testKoIds.foreach { nid =>
+          s1.contains(nid) should be(false)
+          s2.contains(nid) should be(false)
+        }
+      }
+    }
+
+    "on full set" in {
+      val set = SmallIdSet.full[TopicId]
+      for (id <- testAllIds) {
+        val s1 = set + id
+        val s2 = set.added(id)
+        s1 should be(set)
+        s2 should be(set)
+        s1.contains(id) should be(true)
+        s2.contains(id) should be(true)
+        testAllIds.filterNot(_ == id).foreach { nid =>
+          s1.contains(nid) should be(true)
+          s2.contains(nid) should be(true)
+        }
       }
     }
   }
 
-  "added > on non-empty set > existing value" in {
-    val set: SmallIdSet[TopicId] = SmallIdSet(testOkIds*)
-    for (id <- testOkIds) {
-      val s1 = set + id
-      val s2 = set.added(id)
-      s1 should be(set)
-      s2 should be(set)
-      s1.contains(id) should be(true)
-      s2.contains(id) should be(true)
-      testKoIds.foreach { nid =>
-        s1.contains(nid) should be(false)
-        s2.contains(nid) should be(false)
+  "removed" - {
+    "on empty set" in {
+      val set = SmallIdSet.empty[TopicId]
+      for (id <- testOkIds) {
+        val s1 = set - id
+        val s2 = set.removed(id)
+        s1 should be(set)
+        s2 should be(set)
+        s1.contains(id) should be(false)
+        s2.contains(id) should be(false)
+        testAllIds.filterNot(_ == id).foreach { nid =>
+          s1.contains(nid) should be(false)
+          s2.contains(nid) should be(false)
+        }
       }
     }
-  }
 
-  "added > on full set" in {
-    val set = SmallIdSet.full[TopicId]
-    for (id <- testAllIds) {
-      val s1 = set + id
-      val s2 = set.added(id)
-      s1 should be(set)
-      s2 should be(set)
-      s1.contains(id) should be(true)
-      s2.contains(id) should be(true)
+    "on non-empty set > non-existing value" in {
+      val set: SmallIdSet[TopicId] = SmallIdSet(testOkIds*)
+      for (id <- testKoIds) {
+        val s1 = set - id
+        val s2 = set.removed(id)
+        s1 should be(set)
+        s2 should be(set)
+        s1.contains(id) should be(false)
+        s2.contains(id) should be(false)
+        testOkIds.foreach { nid =>
+          s1.contains(nid) should be(true)
+          s2.contains(nid) should be(true)
+        }
+        testKoIds.filterNot(_ == id).foreach { nid =>
+          s1.contains(nid) should be(false)
+          s2.contains(nid) should be(false)
+        }
+      }
+    }
+
+    "on non-empty set > existing value" in {
+      val set: SmallIdSet[TopicId] = SmallIdSet(testOkIds*)
+      for (id <- testOkIds) {
+        val s1 = set - id
+        val s2 = set.removed(id)
+        s1.contains(id) should be(false)
+        s2.contains(id) should be(false)
+        testOkIds.filterNot(_ == id).foreach { nid =>
+          s1.contains(nid) should be(true)
+          s2.contains(nid) should be(true)
+        }
+        testKoIds.foreach { nid =>
+          s1.contains(nid) should be(false)
+          s2.contains(nid) should be(false)
+        }
+      }
+    }
+
+    "on full set" in {
+      val set = SmallIdSet.full[TopicId]
+      for (id <- testAllIds) {
+        val s1 = set - id
+        val s2 = set.removed(id)
+        s1.contains(id) should be(false)
+        s2.contains(id) should be(false)
+        testAllIds.filterNot(_ == id).foreach { nid =>
+          s1.contains(nid) should be(true)
+          s2.contains(nid) should be(true)
+        }
+      }
     }
   }
 }
