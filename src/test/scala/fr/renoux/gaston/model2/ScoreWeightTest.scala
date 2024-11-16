deb3fd02f6b2d8b11330583b726c4bd8619666c7
Gaël Renoux <gael.renoux@gmail.com>
2024-11-16 17:51:01 +0100

    Rename operators due to issue #17158 on Scala 3

--- src/test/scala/fr/renoux/gaston/model2/ScoreWeightTest.scala
+++ src/test/scala/fr/renoux/gaston/model2/ScoreWeightTest.scala
@@ -2,12 +2,13 @@ package fr.renoux.gaston.model2
 
 import fr.renoux.gaston.TestBase
 
+
 class ScoreWeightTest extends TestBase {
 
   "Score*Weight" in {
     val s: Score = 100
     val w: Weight = 0.5
-    (s * w) should be(50)
-    (w * s) should be(50)
+    (s <*> w) should be(50)
+    (w <*> s) should be(50)
   }
 }
