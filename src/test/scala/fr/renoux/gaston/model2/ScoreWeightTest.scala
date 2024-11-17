1d01f50eb4633ead38713b1d782bc71551e8e4f1
Gaël Renoux <gael.renoux@gmail.com>
2024-11-17 19:21:08 +0100

    Fix test

--- src/test/scala/fr/renoux/gaston/model2/ScoreWeightTest.scala
+++ src/test/scala/fr/renoux/gaston/model2/ScoreWeightTest.scala
@@ -8,7 +8,7 @@ class ScoreWeightTest extends TestBase {
   "Score*Weight" in {
     val s: Score = 100
     val w: Weight = 0.5
-    (s <*> w) should be(50)
-    (w <*> s) should be(50)
+    (s * w) should be(50)
+    (w * s) should be(50)
   }
 }
