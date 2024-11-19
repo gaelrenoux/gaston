15ab6239cbec05482c3dccc930a06381ef064197
Gaël Renoux <gael.renoux@gmail.com>
2024-11-19 19:13:46 +0100

    Extract SmallIdSet in separate file

---
+++ src/main/scala/fr/renoux/gaston/model2/smallIdSet.scala
@@ -0,0 +1,68 @@
+package fr.renoux.gaston.model2
+
+import fr.renoux.gaston.util.{Count as _, *}
+
+
+/** SmallIdSet: a set of very small Ids (up to 63) as a single Long. Immutable,
+  * but very cheap to copy.
+  */
+opaque type SmallIdSet[I <: Id] = Long
+
+object SmallIdSet {
+  extension [I >: Int <: Id](s: SmallIdSet[I]) {
+    inline def underlying: Long = s
+
+    inline def apply(id: I): Boolean = contains(id)
+
+    inline def contains(id: I): Boolean =
+      (s & mask(id)) != 0L
+
+    inline def foreach(inline f: I => Unit): Unit = {
+      fastLoop(0, 64) { i =>
+        if (apply(i)) {
+          f(i)
+        }
+      }
+    }
+
+    inline def mapSumToScore(inline f: I => Score): Score = {
+      var result: Score = 0.0
+      foreach { i =>
+        result = result + f(i)
+      }
+      result
+    }
+
+    inline def inserted(id: I): SmallIdSet[I] = {
+      s | mask(id)
+    }
+
+    inline def removed(id: I): SmallIdSet[I] = {
+      s & (~mask(id))
+    }
+
+    // TODO Uncomment this and use them instead of inserted/removed
+    // @targetName("SmallIdSetPlusId")
+    // inline def +(id: I): SmallIdSet[I] = added(id)
+    //
+    // @targetName("SmallIdSetMinusId")
+    // inline def -(id: I): SmallIdSet[I] = removed(id)
+
+    /** Shouldn't be necessary, but type issue otherwise */
+    private inline def mask(id: I): Long = SmallIdSet(id)
+  }
+
+  inline def full[I <: Id]: SmallIdSet[I] = -1
+
+  inline def empty[I <: Id]: SmallIdSet[I] = 0
+
+  inline def apply[I <: Id](id: I): SmallIdSet[I] = 1L << id.value
+
+  inline def apply[I <: Id](ids: I*): SmallIdSet[I] = {
+    var result = 0L
+    ids.fastForeach { id =>
+      result = result | SmallIdSet(id)
+    }
+    result
+  }
+}
