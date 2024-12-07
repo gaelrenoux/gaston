2b6d9ba5efc04dc2972fde1f6d168bea8dce346e
Gaël Renoux <gael.renoux@gmail.com>
2024-12-07 14:10:02 +0100

    Make ScheduleMaker work from an old schedule

---
+++ src/test/scala/fr/renoux/gaston/model2/ScheduleMakerTest.scala
@@ -0,0 +1,15 @@
+package fr.renoux.gaston.model2
+
+import fr.renoux.gaston.TestBase
+import fr.renoux.gaston.input.InputModel
+import fr.renoux.gaston.input.problemFromClassPath
+import fr.renoux.gaston.input.InputTranscription2
+import fr.renoux.gaston.{model as old}
+import fr.renoux.gaston.input.InputLoader
+import fr.renoux.gaston.TestUtils.* 
+
+
+class ScheduleMakerTest extends TestBase {
+
+    // TODO
+}
