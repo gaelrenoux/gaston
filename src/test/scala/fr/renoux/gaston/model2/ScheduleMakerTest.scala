81ab6fd149e808fd245654ef540749aeab5ad475
Gaël Renoux <gael.renoux@gmail.com>
2024-12-07 15:28:50 +0100

    Adds test on ScheduleMaker.fromOld

--- src/test/scala/fr/renoux/gaston/model2/ScheduleMakerTest.scala
+++ src/test/scala/fr/renoux/gaston/model2/ScheduleMakerTest.scala
@@ -6,10 +6,50 @@ import fr.renoux.gaston.input.problemFromClassPath
 import fr.renoux.gaston.input.InputTranscription2
 import fr.renoux.gaston.{model as old}
 import fr.renoux.gaston.input.InputLoader
-import fr.renoux.gaston.TestUtils.* 
+import fr.renoux.gaston.TestUtils.*
+import fr.renoux.gaston.util.Context
 
 
 class ScheduleMakerTest extends TestBase {
+  given Context = Context.Debug
 
-    // TODO
+  "fromOldSchedule" - {
+
+    "basic problem" in {
+      val input: InputModel = InputLoader.fromClassPath("scoring-test.conf").force
+      given oldProblem: old.Problem = problemFromClassPath("scoring-test.conf").force
+      val problem = InputTranscription2(input).result.toEither.force
+      given Printable[Schedule] = SchedulePrinter(problem)
+
+      val oldSchedule: old.Schedule = {
+        val Seq(d1, d2) = oldProblem.slotsList
+        println(s"Topics count: ${oldProblem.topicsList.size}")
+        println(s"Topics: ${oldProblem.topicsList.map(_.name)}")
+        val Seq(alpha, beta, gamma, delta, epsilon2, epsilon1, eta1, eta2, theta, unassignedD1, unassignedD2) =
+          oldProblem.topicsList
+        println(s"Persons count: ${oldProblem.personsList.size}")
+        val Seq(a, b, c, d, e, f, g, h, i, j, k, l) = oldProblem.personsList
+
+        old.Schedule.from(
+          d1(
+            alpha(a, d, e),
+            epsilon1(b, c, f),
+            gamma(g, h, i),
+            eta1(j, k, l)
+          ),
+          d2(
+            beta(b, c, f),
+            epsilon2(a, d, e),
+            delta(g, h, i),
+            eta2(j, k, l)
+          )
+        )
+      }
+      val newSchedule: Schedule = ScheduleMaker.fromOldSchedule(oldSchedule, problem, true)
+      problem.score(newSchedule) should be(Score.Zero)
+      println(newSchedule.toPrettyString)
+    }
+  }
+
+  // TODO More tests required
 }
