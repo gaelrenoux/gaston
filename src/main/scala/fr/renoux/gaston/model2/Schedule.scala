2f66f3aba9bf4df9bb59281266e0ad2ba4f785a7
Gaël Renoux <gael.renoux@gmail.com>
2025-01-02 15:51:34 +0100

    Move score calculation on the schedule (to allow for caching later)

--- src/main/scala/fr/renoux/gaston/model2/Schedule.scala
+++ src/main/scala/fr/renoux/gaston/model2/Schedule.scala
@@ -1,5 +1,9 @@
 package fr.renoux.gaston.model2
 
+import fr.renoux.gaston.util.testOnly
+import fr.renoux.gaston.util.fastForeach
+import fr.renoux.gaston.util.fastFoldRight
+
 // TODO for all of those, check if having an Array isn't better than having a SmallIdSet
 class Schedule(
     val planning: IdMap[SlotId, SmallIdSet[TopicId]],
@@ -37,6 +41,100 @@ class Schedule(
   def isValidFor(pb: SmallProblem): Boolean = {
     // TODO Will be helpful in tests
     ???
+  }
+
+
+  /* ALL SCORING METHODS */
+
+  def score(problem: SmallProblem): Score = {
+    val personalScores: Array[Score] = scorePersons(problem).destructiveSortedValues
+    val personalScoresTotal = personalScores.fastFoldRight(Score.Zero) { (score, acc) =>
+      (SmallProblem.RankFactor * acc: Score) + score
+    }
+    val topicsPureTotal = this.topicsPresent.mapSumToScore(problem.prefsTopicPure(_))
+    personalScoresTotal + topicsPureTotal
+  }
+
+  /** Scores on personsBaseScore, prefsPersonTopic, prefsPersonPerson, prefsTopicsExclusive */
+  // TODO inline this maybe ?
+  def scorePersons(problem: SmallProblem): IdMap[PersonId, Score] = {
+    // TODO to ultra-optimize this, we could have all person scores as a single array: first the base score, then person/topic, then person/person, etc.
+    this.personsToTopics.mapToScore { (pid, topicIds) =>
+      val baseScore = problem.personsBaseScore(pid)
+      val wishesScore = scoreWishes(problem, pid, topicIds)
+      val exclusiveScore = scoreExclusive(problem, pid, topicIds)
+      val linkedScore = scoreLinked(problem, topicIds)
+      baseScore + wishesScore + exclusiveScore + linkedScore
+    }
+  }
+
+  private def scoreWishes(problem: SmallProblem, pid: PersonId, topicIds: SmallIdSet[TopicId]) = {
+    topicIds.mapSumToScore { tid =>
+      val topicsScore = problem.prefsPersonTopic(pid, tid)
+      /* Person antipathy doesn't apply on unassigned topics */
+      val otherPersonsScore: Score = if (tid.value < problem.unassignedTopicsCount.value) 0 else {
+        val otherPersons = this.topicsToPersons(tid) - pid
+        otherPersons.mapSumToScore(problem.prefsPersonPerson(pid, _))
+      }
+      topicsScore + otherPersonsScore
+    }
+  }
+
+  private def scoreExclusive(problem: SmallProblem, pid: PersonId, topicIds: SmallIdSet[TopicId]) =
+    problem.prefsTopicsExclusive(pid).evaluate(topicIds)
+
+  private def scoreLinked(problem: SmallProblem, topicIds: SmallIdSet[TopicId]) = {
+    var linkedScore = Score.Zero
+    problem.prefsTopicsLinked.fastForeach { linked =>
+      val result = linked && topicIds
+      if (result.nonEmpty && result != linked) {
+        linkedScore += Score.MinReward.value * result.size
+      }
+    }
+    linkedScore
+  }
+
+  @testOnly
+  def calculateBaseScores(problem: SmallProblem): Array[Score] = {
+    this.personsToTopics.mapToScore { (pid, topicIds) =>
+      problem.personsBaseScore(pid)
+    }.destructiveSortedValues
+  }
+
+  @testOnly
+  def calculateWishesTopicScores(problem: SmallProblem): Array[Score] = {
+    this.personsToTopics.mapToScore { (pid, topicIds) =>
+      topicIds.mapSumToScore { tid =>
+        problem.prefsPersonTopic(pid, tid)
+      }
+    }.destructiveSortedValues
+  }
+
+  @testOnly
+  def calculateWishesPersonScores(problem: SmallProblem): Array[Score] = {
+    this.personsToTopics.mapToScore { (pid, topicIds) =>
+      topicIds.mapSumToScore { tid =>
+        /* Person antipathy doesn't apply on unassigned topics */
+        if (tid.value < problem.unassignedTopicsCount.value) 0 else {
+          val otherPersons = this.topicsToPersons(tid) - pid
+          otherPersons.mapSumToScore(problem.prefsPersonPerson(pid, _))
+        }
+      }
+    }.destructiveSortedValues
+  }
+
+  @testOnly
+  def calculateExclusiveScores(problem: SmallProblem): Array[Score] = {
+    this.personsToTopics.mapToScore { (pid, topicIds) =>
+      scoreExclusive(problem, pid, topicIds)
+    }.destructiveSortedValues
+  }
+
+  @testOnly
+  def calculateLinkedScores(problem: SmallProblem): Array[Score] = {
+    this.personsToTopics.mapToScore { (pid, topicIds) =>
+      scoreLinked(problem, topicIds)
+    }.destructiveSortedValues
   }
 }
 
