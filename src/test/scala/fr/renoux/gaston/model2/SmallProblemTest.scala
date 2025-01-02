52c41f4b310b208281492ffd40ebc83e72dcd9c2
Gaël Renoux <gael.renoux@gmail.com>
2025-01-02 17:09:12 +0100

    Improves person-wish scoring

--- src/test/scala/fr/renoux/gaston/model2/SmallProblemTest.scala
+++ src/test/scala/fr/renoux/gaston/model2/SmallProblemTest.scala
@@ -1,11 +1,23 @@
 package fr.renoux.gaston.model2
 
 import fr.renoux.gaston.TestBase
+import fr.renoux.gaston.input.InputModel
+import fr.renoux.gaston.input.InputLoader
+import fr.renoux.gaston.input.InputTranscription2
+import fr.renoux.gaston.TestUtils.force
 
 
 class SmallProblemTest extends TestBase {
 
   // TODO test copy
+  // TODO test basic methods
 
+  val input: InputModel = InputLoader.fromClassPath("transcription-test.conf").force
+  val transcription = InputTranscription2(input)
+  val problem = transcription.problem
+
+  "personsWithPersonWish" in {
+    problem.personsWithPersonWish should be(SmallIdSet(0, 2))
+  }
 
 }
