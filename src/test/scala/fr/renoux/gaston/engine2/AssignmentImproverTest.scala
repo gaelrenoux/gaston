3cd0007c686505571cebec719d6e1c65d3b17452
Gaël Renoux <gael.renoux@gmail.com>
2025-01-03 18:57:44 +0100

    Potentially better assignment-improver

--- src/test/scala/fr/renoux/gaston/engine2/AssignmentImproverTest.scala
+++ src/test/scala/fr/renoux/gaston/engine2/AssignmentImproverTest.scala
@@ -22,23 +22,23 @@ class AssignmentImproverTest extends TestBase {
 
     val Seq(d1, d2) = problem.slotsCount.range
     val Seq(unassigned0, unassigned1, alpha, beta, gamma, delta, epsilon1, epsilon2, eta1, eta2, theta) =
-        problem.topicsCount.range
+      problem.topicsCount.range
     val Seq(a, b, c, d, e, f, g, h, i, j, k, l) = problem.personsCount.range
     def scheduleBase() = mkSchedule {
-        d1 slot {
+      d1 slot {
         unassigned0.topicEmpty
         alpha topic (a, d, e) // Alpha, ADE
         epsilon1 topic (b, c, f) // Epsilon #1, BCF
         gamma topic (g, h, i) // Gamma, GHI
         eta1 topic (j, k, l) // Eta ~1, JKL
-        }
-        d2 slot {
+      }
+      d2 slot {
         unassigned1.topicEmpty
         beta topic (b, c, f) // Beta, BCF
         epsilon2 topic (a, d, e) // Epsilon #2, ADE,
         delta topic (g, h, i) // Delta, GHI
         eta2 topic (j, k, l) // Eta ~2, JKL
-        }
+      }
     }
     println(scheduleBase().toPrettyString)
     val baseScore = scheduleBase().score(problem)
@@ -46,14 +46,42 @@ class AssignmentImproverTest extends TestBase {
 
     val improver = new AssignmentImprover(problem)
 
-    "Basic" in {
-        given Random = new Random(0)
-        val schedule = scheduleBase()
-        improver.improve(schedule)
-        val newScore = schedule.score(problem)
-        newScore should be > baseScore
-        println(schedule.toPrettyString)
-        println(s"Score: $newScore")
+    "Improved schedule should be better than the base one" in {
+      given Random = new Random(0)
+      val schedule = scheduleBase()
+      improver.improve(schedule)
+      val newScore = schedule.score(problem)
+      newScore should be > baseScore
+      println(schedule.toPrettyString)
+      println(s"Score: $newScore")
+    }
+
+    "Repeated improvements should do nothing (usually)" in {
+      given Random = new Random(0)
+      val schedule = scheduleBase()
+      improver.improve(schedule)
+      val newScore = schedule.score(problem)
+      newScore should be > baseScore
+      //println(schedule.toPrettyString)
+      //println(s"Score: $newScore")
+      improver.improve(schedule)
+      val newNewScore = schedule.score(problem)
+      newNewScore should be(newScore)
+      //println(schedule.toPrettyString)
+      //println(s"Score: $newScore")
+    }
+
+    "Multiple runs should produce about the same result (usually)" in {
+      given Random = new Random(0)
+      val schedule1 = scheduleBase()
+      improver.improve(schedule1)
+      val schedule2 = scheduleBase()
+      improver.improve(schedule2)
+      val score1 = schedule1.score(problem)
+      val score2 = schedule2.score(problem)
+      score1 should be (score2)
+      //println(schedule1.toPrettyString)
+      //println(schedule2.toPrettyString)
     }
 
   }
