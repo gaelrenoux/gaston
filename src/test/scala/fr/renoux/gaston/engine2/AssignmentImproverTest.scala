b463a131957d4ed1bf044ac09867167d3712d488
Gaël Renoux <gael.renoux@gmail.com>
2025-01-03 12:49:02 +0100

    First draft of assigment-improver for V2 model

---
+++ src/test/scala/fr/renoux/gaston/engine2/AssignmentImproverTest.scala
@@ -0,0 +1,61 @@
+package fr.renoux.gaston.engine2
+
+import fr.renoux.gaston.input.InputModel
+import fr.renoux.gaston.input.InputLoader
+import fr.renoux.gaston.input.InputTranscription2
+import fr.renoux.gaston.TestBase
+import fr.renoux.gaston.TestUtils.force
+import fr.renoux.gaston.model2.ScheduleMaker.mkSchedule
+import fr.renoux.gaston.model2.*
+import fr.renoux.gaston.util.Context
+import scala.util.Random
+
+
+class AssignmentImproverTest extends TestBase {
+  given Context = Context.Default
+
+  "Nominal case" - {
+    val input: InputModel = InputLoader.fromClassPath("scoring-test.conf").force
+    val problem = InputTranscription2(input).result.toEither.force
+    import problem.given
+    given SchedulePrinter = new SchedulePrinter(problem)
+
+    val Seq(d1, d2) = problem.slotsCount.range
+    val Seq(unassigned0, unassigned1, alpha, beta, gamma, delta, epsilon1, epsilon2, eta1, eta2, theta) =
+        problem.topicsCount.range
+    val Seq(a, b, c, d, e, f, g, h, i, j, k, l) = problem.personsCount.range
+    def scheduleBase() = mkSchedule {
+        d1 slot {
+        unassigned0.topicEmpty
+        alpha topic (a, d, e) // Alpha, ADE
+        epsilon1 topic (b, c, f) // Epsilon #1, BCF
+        gamma topic (g, h, i) // Gamma, GHI
+        eta1 topic (j, k, l) // Eta ~1, JKL
+        }
+        d2 slot {
+        unassigned1.topicEmpty
+        beta topic (b, c, f) // Beta, BCF
+        epsilon2 topic (a, d, e) // Epsilon #2, ADE,
+        delta topic (g, h, i) // Delta, GHI
+        eta2 topic (j, k, l) // Eta ~2, JKL
+        }
+    }
+    println(scheduleBase().toPrettyString)
+    val baseScore = scheduleBase().score(problem)
+    println(s"Score: $baseScore")
+
+    val improver = new AssignmentImprover(problem)
+
+    "Basic" in {
+        given Random = new Random(0)
+        val schedule = scheduleBase()
+        improver.improve(schedule)
+        val newScore = schedule.score(problem)
+        newScore should be > baseScore
+        println(schedule.toPrettyString)
+        println(s"Score: $newScore")
+    }
+
+  }
+
+}
