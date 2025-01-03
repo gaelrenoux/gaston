19f22c9d69601ba238aaab319e991d442cfed59b
Gaël Renoux <gael.renoux@gmail.com>
2025-01-03 18:57:44 +0100

    Potentially better assignment-improver

--- src/main/scala/fr/renoux/gaston/engine2/AssignmentImprover.scala
+++ src/main/scala/fr/renoux/gaston/engine2/AssignmentImprover.scala
@@ -27,88 +27,116 @@ final class AssignmentImprover(private val problem: SmallProblem)(using private 
     */
   def improve(schedule: Schedule, maxRounds: Int = defaultMaxRoundsCount)(using Random) = 
     chrono("PersonPlacementImprover >  improve") {
-      val randTopicIds = schedule.topicsPresent.toShuffledArray
-      val personIds = problem.personsCount.shuffled
+      val slotsToChangeablePersons: IdMap[SlotId, Array[PersonId]] =
+        IdMap.tabulate[SlotId, Array[PersonId]] { sid =>
+          val personsWhoCanMove = problem.slotsPersonsPresent(sid).filter { pid =>
+            val tid = schedule.topicOf(sid, pid)
+            !problem.isPersonMandatory(pid, tid)
+          }
+          personsWhoCanMove.toShuffledArray
+        }
+      val slotsToTopics: IdMap[SlotId, Array[TopicId]] = schedule.slotsToTopics.mapValues(_.toShuffledArray)
 
-      var personIx = 0
-      var blockedPersons = 0
       var roundsLeft = maxRounds
+      var sid: SlotId = 0
+      var blockedSlots = 0
 
-      while (blockedPersons < problem.personsCount.value && roundsLeft > 0) {
-        val pid = personIds(personIx)
-        if (goodChangeForPerson(schedule, pid, randTopicIds)) {
-          blockedPersons = 0
-        } else {
-          blockedPersons += 1
+      while(blockedSlots < problem.slotsCount && roundsLeft > 0) { // iterate over slots
+        val randPersonIds = slotsToChangeablePersons(sid)
+        val randTopicIds = slotsToTopics(sid)
+
+        var changesMade = false
+        var subRoundsLeft = math.min(roundsLeft, defaultMaxSuccessiveRoundsCountPerSlot)
+        var personIx = 0
+        var blockedPersons = 0
+
+        while (blockedPersons < randPersonIds.length && subRoundsLeft > 0) { // iterate over persons
+          val pid = randPersonIds(personIx)
+          var targetTopicIx = 0
+          var foundGoodChange = false
+          while (!foundGoodChange && targetTopicIx < randTopicIds.length) {
+            val targetTid = randTopicIds(targetTopicIx)
+            foundGoodChange = goodChangeForPersonTopic(schedule, pid, targetTid)
+            targetTopicIx += 1
+          }
+
+          if (foundGoodChange) {
+            // We changed something on the current slot, making new changes possible
+            blockedPersons = 0
+            changesMade = true
+          } else {
+            // Can't improve the current person anymore, until something else changes
+            blockedPersons += 1
+          }
+
+          personIx = (personIx + 1) % randPersonIds.length
+          subRoundsLeft -= 1
+          roundsLeft -= 1
         }
 
-        personIx = (personIx + 1) % problem.personsCount.value
-        roundsLeft -= 1
+        if (changesMade) {
+            // We changed something on the current slots, making new changes possible (but this slot is done pending further changes)
+          blockedSlots = 1
+        } else {
+            // Couldn't improve the current slot
+          blockedSlots += 1
+        }
+
+        sid = sid.next
       }
+
+
     }
 
-  /** Try to improve the schedule for that person */
-  private def goodChangeForPerson(schedule: Schedule, pid: PersonId, randTopicIds: Array[TopicId]): Boolean = {
-      var targetTopicIx = 0
-      var found = false
 
-      while (!found && targetTopicIx < randTopicIds.length) {
-        val targetTid = randTopicIds(targetTopicIx)
-        val targetTopicPersons = schedule.topicsToPersons(targetTid)
-        if (!targetTopicPersons.contains(pid)) {
-          // TODO If a person is locked for a slot because it's on a mandatory topic, we're still going to try to move them => let's go slot by slot instead
-          found = goodChangeForPersonTopic(schedule, pid, targetTid, targetTopicPersons)
+  private def goodChangeForPersonTopic(schedule: Schedule, pid: PersonId, targetTid: TopicId): Boolean = {
+    val targetTopicPersons = schedule.topicsToPersons(targetTid)
+    if (targetTopicPersons.contains(pid)) false
+    else {
+      var found = false
+      val sid = schedule.topicsToSlot(targetTid)
+      val currentTid = schedule.topicOf(sid, pid)
+
+      if (currentTid != TopicId.Absent && !problem.isPersonMandatory(pid, currentTid)) {
+
+        /* First, let's see if we can just move the person onto the target topic */
+        if (schedule.isDroppableTopic(problem, pid, currentTid) && schedule.isAddableTopic(problem, pid, targetTid)) {
+          /* We can just move that person on the target topic */
+          val oldScore = schedule.score(problem)
+          schedule.move(pid, currentTid, targetTid)
+          val newScore = schedule.score(problem)
+          if (newScore <= oldScore) {
+            val _ = schedule.reverseMove(pid, currentTid, targetTid)
+          } else {
+            // found a good one
+            found = true
+          }
         }
-        targetTopicIx += 1
+
+        /* If moving the person wasn't possible or didn't improve the score, we'll try to swap the person with another one */
+        if (!found) {
+          targetTopicPersons.foreachWhile { otherPid =>
+            if (!problem.isPersonMandatory(otherPid, targetTid)) {
+              val oldScore = schedule.score(problem)
+              schedule.move(pid, currentTid, targetTid)
+              schedule.move(otherPid, targetTid, currentTid)
+              val newScore = schedule.score(problem)
+              if (newScore <= oldScore) {
+                schedule.reverseMove(otherPid, targetTid, currentTid)
+                schedule.reverseMove(pid, currentTid, targetTid)
+                ()
+              } else {
+                // found a good one
+                found = true
+              }
+            }
+            !found
+          }
+        }
       }
 
       found
-  }
-
-  private def goodChangeForPersonTopic(schedule: Schedule, pid: PersonId, targetTid: TopicId, targetTopicPersons: SmallIdSet[PersonId]): Boolean = {
-    var found = false
-    val sid = schedule.topicsToSlot(targetTid)
-    val currentTid = schedule.topicOf(sid, pid)
-
-    if (!problem.isPersonMandatory(pid, currentTid)) {
-
-      /* First, let's see if we can just move the person onto the target topic */
-      if (schedule.isDroppableTopic(problem, pid, currentTid) && schedule.isAddableTopic(problem, pid, targetTid)) {
-        /* We can just move that person on the target topic */
-        val oldScore = schedule.score(problem)
-        schedule.move(pid, currentTid, targetTid)
-        val newScore = schedule.score(problem)
-        if (newScore <= oldScore) {
-          val _ = schedule.reverseMove(pid, currentTid, targetTid)
-        } else {
-          // found a good one
-          found = true
-        }
-      }
-      
-      /* If moving the person wasn't possible or didn't improve the score, we'll try to swap the person with another one */
-      if (!found) {
-        targetTopicPersons.foreachWhile { otherPid =>
-          if (!problem.isPersonMandatory(otherPid, targetTid)) {
-            val oldScore = schedule.score(problem)
-            schedule.move(pid, currentTid, targetTid)
-            schedule.move(otherPid, targetTid, currentTid)
-            val newScore = schedule.score(problem)
-            if (newScore <= oldScore) {
-              schedule.reverseMove(otherPid, targetTid, currentTid)
-              schedule.reverseMove(pid, currentTid, targetTid)
-              ()
-            } else {
-              // found a good one
-              found = true
-            }
-          }
-          !found
-        }
-      }
     }
-    
-    found
   }
 
   // TODO From previous version:
