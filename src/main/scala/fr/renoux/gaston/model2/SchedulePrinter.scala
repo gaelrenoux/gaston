3287897f9fae8c4116fc3a0c51bc6510d47166a9
Gaël Renoux <gael.renoux@gmail.com>
2025-02-16 17:15:30 +0100

    Fix SchedulePrinter

--- src/main/scala/fr/renoux/gaston/model2/SchedulePrinter.scala
+++ src/main/scala/fr/renoux/gaston/model2/SchedulePrinter.scala
@@ -3,18 +3,25 @@ package fr.renoux.gaston.model2
 class SchedulePrinter(problem: SmallProblem) extends Printable[Schedule] {
   import problem.given
 
+  // TODO Missing mandatory markers, plus the mandatory person should always come first
+
   extension (schedule: Schedule)
     override def toPrettyString: String = {
       val sb = new StringBuilder
       problem.slotsCount.foreach { sid =>
         sb.append(problem.slotsNames(sid)).append("\n")
-//        schedule.slotsToTopics(sid).foreach { tid =>
-//          sb.append("    ").append(problem.topicsName(tid)).append(" ==> ")
-//          val persons = schedule.topicsToPersons(tid).toSet
-//          val personNames = persons.map(problem.personsName.apply).toSeq.sorted
-//          sb.append(personNames.mkString(", ")).append("\n")
-//          ()
-//        }
+        val assignment = schedule.slotsToAssignment(sid)
+        val topics = schedule.slotsToTopics(sid)
+        topics.foreach { tid =>
+          sb.append("    ").append(problem.topicsName(tid)).append(" ==>")
+          val persons = assignment.topicsToPersons(tid).toSet
+          if (persons.nonEmpty) {
+            val personNames = persons.map(problem.personsName.apply).toSeq.sorted
+            sb.append(" ").append(personNames.mkString(", "))
+          }
+          sb.append("\n")
+          ()
+        }
       }
       val unscheduled = problem.topicsCount.range.toSet -- schedule.topicsPresent.toSet
       sb.append("Unscheduled topics: ").append(unscheduled.map(problem.topicsName.apply).toSeq.sorted.mkString(", "))
