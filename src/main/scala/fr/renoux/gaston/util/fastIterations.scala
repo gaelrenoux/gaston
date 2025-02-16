1018dac3bccdb3b012319aa51d2d6eeaf6487462
Gaël Renoux <gael.renoux@gmail.com>
2025-02-16 15:28:46 +0100

    More tests and utility cleanup

--- src/main/scala/fr/renoux/gaston/util/fastIterations.scala
+++ src/main/scala/fr/renoux/gaston/util/fastIterations.scala
@@ -2,6 +2,38 @@ package fr.renoux.gaston.util
 
 import scala.reflect.ClassTag
 
+/** Simple generic loop */
+inline def fastLoop[A](inline start: A, inline condition: A => Boolean, inline advance: A => A)(
+    inline loopBody: A => Any
+): Unit = {
+  var a = start
+  while (condition(a)) {
+    loopBody(a)
+    a = advance(a)
+  }
+}
+
+/** Simple loop for ranges */
+inline def fastLoop(inline start: Int, inline until: Int, inline advance: Int => Int = _ + 1)(
+    inline loopBody: Int => Any
+): Unit = {
+  var i = start
+  while (i < until) {
+    loopBody(i)
+    i = advance(i)
+  }
+}
+
+/** Loop that stops when it found the correct integer. Default value is required to avoid using an Option. */
+inline def fastFind(inline start: Int, inline until: Int, inline advance: Int => Int = _ + 1, inline default: Int = -1)(
+    inline loopBody: Int => Boolean
+): Int = {
+  var i = start
+  while (!loopBody(i) && i < until) {
+    i = advance(i)
+  }
+  if (i >= until) default else i
+}
 
 /** Array extension */
 extension [A](inline as: Array[A]) {
