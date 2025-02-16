69f9dda13f5ab66d8606a1342b5b866145b548c3
Gaël Renoux <gael.renoux@gmail.com>
2025-02-16 11:05:51 +0100

    Better cache

--- src/test/scala/fr/renoux/gaston/model2/ScheduleTest.scala
+++ src/test/scala/fr/renoux/gaston/model2/ScheduleTest.scala
@@ -12,17 +12,19 @@ class ScheduleTest extends TestBase {
 
   "Basics " ignore { // TODO fix that, we need a way to create a simple problem
     given countSlots: CountAll[SlotId] = CountAll[SlotId](2)
+
     given countTopics: CountAll[TopicId] = CountAll[TopicId](4)
+
     given countPersons: CountAll[PersonId] = CountAll[PersonId](4)
     import ScheduleMaker.mkSchedule
 
     val schedule = mkSchedule(???) {
       0 slot {
-        0 topic (0, 1, 2)
+        0 topic(0, 1, 2)
         1 topic (3)
       }
       1 slot {
-        3 topic (0, 1, 2, 3)
+        3 topic(0, 1, 2, 3)
       }
     }
 
@@ -49,6 +51,7 @@ class ScheduleTest extends TestBase {
 
   "Scoring" - {
     val input: InputModel = InputLoader.fromClassPath("scoring-test.conf").force
+
     given problem: SmallProblem = InputTranscription2(input).result.toEither.force
     import problem.given
     given SchedulePrinter = new SchedulePrinter(problem)
@@ -57,22 +60,24 @@ class ScheduleTest extends TestBase {
     val Seq(unassigned0, unassigned1, alpha, beta, gamma, delta, epsilon1, epsilon2, eta1, eta2, theta) =
       problem.topicsCount.range
     val Seq(a, b, c, d, e, f, g, h, i, j, k, l) = problem.personsCount.range
+
     def scheduleBase(myProblem: SmallProblem = problem) = mkSchedule(myProblem) {
       d1 slot {
         unassigned0.topicEmpty
-        alpha topic (a, d, e) // Alpha, ADE
-        epsilon1 topic (b, c, f) // Epsilon #1, BCF
-        gamma topic (g, h, i) // Gamma, GHI
-        eta1 topic (j, k, l) // Eta ~1, JKL
+        alpha topic(a, d, e) // Alpha, ADE
+        epsilon1 topic(b, c, f) // Epsilon #1, BCF
+        gamma topic(g, h, i) // Gamma, GHI
+        eta1 topic(j, k, l) // Eta ~1, JKL
       }
       d2 slot {
         unassigned1.topicEmpty
-        beta topic (b, c, f) // Beta, BCF
-        epsilon2 topic (a, d, e) // Epsilon #2, ADE,
-        delta topic (g, h, i) // Delta, GHI
-        eta2 topic (j, k, l) // Eta ~2, JKL
+        beta topic(b, c, f) // Beta, BCF
+        epsilon2 topic(a, d, e) // Epsilon #2, ADE,
+        delta topic(g, h, i) // Delta, GHI
+        eta2 topic(j, k, l) // Eta ~2, JKL
       }
     }
+
     println(scheduleBase().toPrettyString)
     val personScoresBase: Map[PersonId, Score] = problem.personsCount.range.map(_ -> Score.Zero).toMap
 
@@ -164,6 +169,21 @@ class ScheduleTest extends TestBase {
     }
     // TODO test weight on other scores as well
 
+    "multiple moves in a row reset cache correctly" in {
+      val schedule = scheduleBase()
+      schedule.getTotalScore() should be(Score.Zero)
+      schedule.on(d1)(_.move(f, epsilon1, alpha))
+      schedule.getPersonScores().toMap should be(
+        personScoresBase + (f -> 750)
+      )
+      schedule.getTotalScore() should be(750.0 / 2048)
+      schedule.on(d1)(_.move(h, gamma, alpha))
+      schedule.on(d2)(_.move(h, delta, beta))
+      schedule.getPersonScores().toMap should be(
+        personScoresBase + (c -> 500) + (f -> 750)
+      )
+      schedule.getTotalScore() should be(750.0 / 2048 + 500.0 / 1024)
+    }
   }
 
 }
